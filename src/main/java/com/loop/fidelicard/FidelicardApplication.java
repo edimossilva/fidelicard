d763d72faab094df4ff8f1849c75f1663fbd9644
edimossilva <edimossilva@gmail.com>
2018-06-19 19:10:56 -0300

    first commit

---
+++ src/main/java/com/loop/fidelicard/FidelicardApplication.java
@@ -0,0 +1,12 @@
+package com.loop.fidelicard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelicardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelicardApplication.class, args);
+	}
+}
