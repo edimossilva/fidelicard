c1a5755dbd7a2b2350f80c894ce80f905cf78f3a
edimossilva <edimossilva@gmail.com>
2018-06-24 16:09:05 -0300

    adding swagger

--- src/main/java/com/loop/fidelicard/FidelicardApplication.java
+++ src/main/java/com/loop/fidelicard/FidelicardApplication.java
@@ -1,21 +1,13 @@
 package com.loop.fidelicard;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.loop.fidelicard.model.Stamp;
-import com.loop.fidelicard.repository.StampRepository;
 
 @SpringBootApplication
 public class FidelicardApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(FidelicardApplication.class, args);
 	}
 
-	
 }
