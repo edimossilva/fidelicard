fdfacfdc419bf40385e92f0ab8c6c30609d8a841
edimossilva <edimossilva@gmail.com>
2018-07-29 22:18:59 -0300

    adding changes in response token dto

---
+++ src/main/java/com/loop/fidelicard/security/dto/ResponseTokenDTO.java
@@ -0,0 +1,16 @@
+package com.loop.fidelicard.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Builder
+@AllArgsConstructor
+public class ResponseTokenDTO {
+	private String token;
+}
--- src/main/java/com/loop/fidelicard/security/jwt/filter/TokenAuthenticationService.java
+++ src/main/java/com/loop/fidelicard/security/jwt/filter/TokenAuthenticationService.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loop.fidelicard.security.dto.ResponseTokenDTO;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -25,13 +29,28 @@ public class TokenAuthenticationService {
 				.signWith(SignatureAlgorithm.HS512, SECRET).compact();
 
 		String token = TOKEN_PREFIX + " " + JWT;
-		res.addHeader(HEADER_STRING, token);
+
+		String jsonInString = tokenToJson(token);
+		res.addHeader(HEADER_STRING, jsonInString);
 
 		try {
-			res.getOutputStream().print(token);
+			res.getOutputStream().print(jsonInString);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static String tokenToJson(String token) {
+		ObjectMapper mapper = new ObjectMapper();
+		ResponseTokenDTO dto = new ResponseTokenDTO(token);
+		String jsonInString = null;
+
+		try {
+			jsonInString = mapper.writeValueAsString(dto);
+		} catch (JsonProcessingException e1) {
+			e1.printStackTrace();
+		}
+		return jsonInString;
 	}
 
 	public static Authentication getByToken(String token) {
