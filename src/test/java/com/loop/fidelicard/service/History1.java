5de070826f5d65620c015f7536f51010c921e66b
edimossilva <edimossilva@gmail.com>
2018-08-07 23:37:29 -0300

    increasing token time

---
+++ src/test/java/com/loop/fidelicard/service/History1.java
@@ -0,0 +1,52 @@
+package com.loop.fidelicard.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.loop.fidelicard.dto.finalclient.ResponseFinalClientDTO;
+import com.loop.fidelicard.dto.hybrid.ClientUIAndEnterpriseIdDTO;
+import com.loop.fidelicard.mock.MyMock;
+import com.loop.fidelicard.security.model.LoginUserService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class History1 {
+	@Autowired
+	FinalClientService finalClientService;
+	@Autowired
+	EnterpriseService enterpriseService;
+	@Autowired
+	LoginUserService loginUserService;
+	@Autowired
+	OfferService offerService;
+	@Autowired
+	CardService cardService;
+	@Autowired
+	StampService stampService;
+
+	@Before
+	public void before() {
+		MyMock.createLoginUser1(loginUserService);
+		MyMock.createEnterprise1(loginUserService, enterpriseService);
+		MyMock.createOffer1(offerService, enterpriseService);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testFindClientResponseDTOByUIAndEnterpriseIdWhenNotExist() {
+		ClientUIAndEnterpriseIdDTO dto = new ClientUIAndEnterpriseIdDTO();
+		dto.setFinalClientUI("new UI");
+		dto.setEnterpriseId(MyMock.getEnterprise().getId());
+		finalClientService.findClientResponseDTOByUIAndEnterpriseId(dto);
+	}
+}
